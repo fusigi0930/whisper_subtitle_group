0a90c55cd1acbbd9a5993e7182e329e82c166603
ChuYuan Chiang <chuyuan.chiang@gmail.com>
2024-07-06 22:05:07 +0800

    add a feature to translate a srt file to the other language e.g. python st_group --file xxx.srt

--- st_group.py
+++ st_group.py
@@ -5,14 +5,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--voice", help = "set voice languge", default="jp")
     parser.add_argument("-l", "--lang", help = "set output srt file langue", default="zh")
-    parser.add_argument("-f", "--file", help = "video file")
+    parser.add_argument("-f", "--file", help = "file")
     args = parser.parse_args()
 
     st = subtitle.Subtitle()
     st.set_lang(args.voice)
-    st.process(args.file)
-    st.store_to_srt()
-    st.store_to_srt(args.lange)
+    if args.file.split(".")[-1] == "srt":
+        st.store_to_srt_from_srt(args.file, args.lang)
+    else:
+        st.process(args.file)
+        st.store_to_srt()
+        st.store_to_srt(args.lang)
+
     st.close()
 
 if __name__ == "__main__":
